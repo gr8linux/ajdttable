adea50bdbdbfa20d98a81875cfb3e4e0214bfece
shajeebs <shajeeb.s@gmail.com>
2020-09-01 12:58:58 +0100

    Initial Commit
    Initial Commit

---
+++ ajaxtable.php
@@ -0,0 +1,301 @@
+<?php
+/*
+Plugin Name: AjaxTable
+Description: AjaxTable is multipurpose data table used for displaying custom data from the database.
+Version: 1.1.1
+Author: shajeeb
+Author URI: https://nimra-tech.com/
+Text Domain: nimratech
+License: GPL2
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+*/
+
+/**
+ * AjaxUtility class
+ *
+ * @class AjaxUtility The class that holds the entire AjaxUtility plugin
+ */
+final class AjaxUtility {
+    /**
+     * Plugin version
+     *
+     * @var string
+     */
+    public $version = '1.1.1';
+
+    /**
+     * Holds various class instances
+     *
+     * @var array
+     */
+    private $container = [];
+  
+    /**
+     * Initializes the AjaxUtility() class
+     *
+     * Checks for an existing AjaxUtility() instance
+     * and if it doesn't find one, creates it.
+     */
+    public static function init() {
+        static $instance = false;
+
+        if ( ! $instance ) {
+            $instance = new AjaxUtility();
+        }
+
+        return $instance;
+    }
+
+    /**
+     * Magic getter to bypass referencing plugin.
+     *
+     * @param $prop
+     *
+     * @return mixed
+     */
+    public function __get( $prop ) {
+        if ( array_key_exists( $prop, $this->container ) ) {
+            return $this->container[ $prop ];
+        }
+
+        return $this->{$prop};
+    }
+
+    /**
+     * Magic isset to bypass referencing plugin.
+     *
+     * @param $prop
+     *
+     * @return mixed
+     */
+    public function __isset( $prop ) {
+        return isset( $this->{$prop} ) || isset( $this->container[ $prop ] );
+    }
+
+    /**
+     * Define the constants
+     *
+     * @return void
+     */
+    public function define_constants() {
+        define( 'AJDT_VERSION', $this->version );
+        define( 'AJDT_FILE', __FILE__ );
+        define( 'AJDT_PATH', dirname( AJDT_FILE ) );
+        define( 'AJDT_INCLUDES', AJDT_PATH . '/includes' );
+        define( 'AJDT_URL', plugins_url( '', AJDT_FILE ) );//plugin_dir_url( __FILE__ )
+        define( 'AJDT_ASSETS', AJDT_URL . '/assets' );
+    }
+
+    /**
+     * Placeholder for activation function
+     *
+     * Nothing being called here yet.
+     */
+    public function activate() {
+        global $wpdb;
+        $table_Utils = $wpdb->prefix.'ajdt_utils'; 
+
+        // Utils details
+        $sql = "CREATE TABLE IF NOT EXISTS $table_Utils (
+            id int(11) AUTO_INCREMENT PRIMARY KEY,
+            name     varchar(50) NOT NULL,
+            age     INT NOT NULL,
+            email varchar(50)  not null,
+            place     varchar(50) NOT NULL,
+            created_at date NOT NULL);";
+
+        require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+        dbDelta($sql);
+
+        $data = [
+            ['name' => 'Tarek, Rahman', 'age' => 22, 'email' => 'tarek.rahman@refin.com', 'place' => 'Nottingham', "created_at" => date("Y-m-d")],
+            ['name' => 'Farish, George W', 'age' => 21, 'email' => 'george.farish@refin.com', 'place' => 'London', "created_at" => date("Y-m-d")],
+            ['name' => 'Arjun, Menon', 'age' => 43, 'email' => 'arjun.menon@refin.com', 'place' => 'Birmingham', "created_at" => date("Y-m-d")],
+            ['name' => 'Charles-Love, Nadege', 'age' => 55, 'email' => 'nadege.charles@refin.com', 'place' => 'Liverpool', "created_at" => date("Y-m-d")],
+            ['name' => 'Wood, Claire', 'age' => 37, 'email' => 'claire.wood@refin.com', 'place' => 'Manchestor', "created_at" => date("Y-m-d")],
+            ['name' => 'Dowell,  Campbell', 'age' => 28, 'email' => 'campbell.dowell@refin.com', 'place' => 'Sounthampton', "created_at" => date("Y-m-d")],
+            ['name' => 'Telenkov, Evgenii', 'age' => 43, 'email' => 'evgenii.telenkov@refin.com', 'place' => 'Derby', "created_at" => date("Y-m-d")],
+            ['name' => 'Davidson, Brian', 'age' => 65, 'email' => 'brian.davidson@refin.com', 'place' => 'Licestor', "created_at" => date("Y-m-d")],
+            ['name' => 'Henry, Alan', 'age' => 33, 'email' => 'alan.henry@refin.com', 'place' => 'Beeston', "created_at" => date("Y-m-d")],
+        ];
+
+        foreach ($data as $stud) {
+                $result = $wpdb->insert($table_Utils, $stud);
+        }
+        
+    }
+
+    /**
+     * Placeholder for deactivation function
+     *
+     * Nothing being called here yet.
+     */
+    public function deactivate() {
+        global $wpdb;
+        $table_Utils = $wpdb->prefix.'ajdt_utils'; 
+
+        // Removing Utils details
+        $sql = "DROP TABLE $table_Utils;";
+
+        require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+        dbDelta($sql);
+    }
+
+    /**
+     * Constructor for the AjaxUtility class
+     *
+     * Sets up all the appropriate hooks and actions
+     * within our plugin.
+     */
+    public function __construct() {
+        $this->define_constants();
+
+        register_activation_hook( __FILE__, array( $this, 'activate' ) );
+        register_deactivation_hook( __FILE__, array( $this, 'deactivate' ) );
+        add_action( 'init', array( $this, 'init_plugin' ) );
+        //add_action( 'init', array( $this, 'localization_setup' ) );
+    }
+
+    /**
+     * Load the plugin after all plugis are loaded
+     *
+     * @return void
+     */
+    public function init_plugin() {
+        $this->includes();
+        //$this->init_hooks();
+        $this->init_classes();
+        do_action( 'ajdt_loaded' );
+    }
+
+    /**
+     * Include the required files
+     *
+     * @return void
+     */
+    public function includes() {
+        $namespace = '/ajdt/v1/';
+        include_once AJDT_PATH . '/class-assets.php';
+        include_once AJDT_PATH . '/class-admin.php';
+// if ( $this->is_request( 'admin' ) ) { } require_once include_once
+        // if ( $this->is_request( 'frontend' ) ) { }
+        // if ( class_exists( 'WeDevs_Dokan' ) ) { }
+
+        include_once AJDT_PATH . '/class-api-utils.php';
+    }
+
+    /**
+     * Instantiate the required classes
+     *
+     * @return void
+     */
+    public function init_classes() {
+        // if ( $this->is_request( 'admin' ) ) { }
+        // if ( $this->is_request( 'frontend' ) ) { }
+        // if ( class_exists( 'WeDevs_Dokan' ) ) { }
+
+        //$this->container['rest'] = new AjaxTable\REST_API();
+        $this->container['admin'] = new AjaxTable\Admin();
+        $this->container['assets'] = new AjaxTable\Assets();
+    }
+
+    /**
+     * Initialize plugin for localization
+     *
+     * @uses load_plugin_textdomain()
+     */
+    public function localization_setup() {
+        load_plugin_textdomain( 'ajdt', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
+    }
+
+} // class AjaxUtility
+$ajdt = AjaxUtility::init();
+
+/*
+function ajdt_install() {
+    global $wpdb;
+    $table_Utils = $wpdb->prefix.'ajdt_utils'; 
+
+    // Utils details
+    $sql = "CREATE TABLE IF NOT EXISTS $table_Utils (
+        id int(11) AUTO_INCREMENT PRIMARY KEY,
+        name     varchar(50) NOT NULL,
+        age     INT NOT NULL,
+        email varchar(50)  not null,
+        place     varchar(50) NOT NULL,
+        created_at date NOT NULL);";
+
+    require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+    dbDelta($sql);
+    
+}
+register_activation_hook(__FILE__, 'ajdt_install');
+
+function ajdt_install_data(){
+    global $wpdb;
+    $table_Utils = $wpdb->prefix.'ajdt_utils'; 
+
+    $data = [
+        ['name' => 'Tarek, Rahman', 'age' => 22, 'email' => 'tarek.rahman@refin.com', 'place' => 'Nottingham', "created_at" => date("Y-m-d")],
+        ['name' => 'Farish, George W', 'age' => 21, 'email' => 'george.farish@refin.com', 'place' => 'London', "created_at" => date("Y-m-d")],
+        ['name' => 'Arjun, Menon', 'age' => 43, 'email' => 'arjun.menon@refin.com', 'place' => 'Birmingham', "created_at" => date("Y-m-d")],
+        ['name' => 'Charles-Love, Nadege', 'age' => 55, 'email' => 'nadege.charles@refin.com', 'place' => 'Liverpool', "created_at" => date("Y-m-d")],
+        ['name' => 'Wood, Claire', 'age' => 37, 'email' => 'claire.wood@refin.com', 'place' => 'Manchestor', "created_at" => date("Y-m-d")],
+        ['name' => 'Dowell,  Campbell', 'age' => 28, 'email' => 'campbell.dowell@refin.com', 'place' => 'Sounthampton', "created_at" => date("Y-m-d")],
+        ['name' => 'Telenkov, Evgenii', 'age' => 43, 'email' => 'evgenii.telenkov@refin.com', 'place' => 'Derby', "created_at" => date("Y-m-d")],
+        ['name' => 'Davidson, Brian', 'age' => 65, 'email' => 'brian.davidson@refin.com', 'place' => 'Licestor', "created_at" => date("Y-m-d")],
+        ['name' => 'Henry, Alan', 'age' => 33, 'email' => 'alan.henry@refin.com', 'place' => 'Beeston', "created_at" => date("Y-m-d")],
+    ];
+
+    foreach ($data as $stud) {
+            $result = $wpdb->insert($table_Utils, $stud);
+    }
+}
+register_activation_hook(__FILE__, 'ajdt_install_data');
+*/
+
+//Register the API routes for the objects of the controller.
+add_action( 'rest_api_init', function () {
+    require_once(plugin_dir_path(__FILE__).'/class-api-utils.php');
+    $ctrl = new ClassApiUtils();
+    $ctrl->register_routes();
+});
+
+// function ajdt_enqueue_vuejs_styles() {
+//     wp_enqueue_style('bootstrap-vue', '//unpkg.com/bootstrap/dist/css/bootstrap.min.css');
+//     wp_enqueue_style('bootstrap-vue-min', '//unpkg.com/bootstrap-vue@latest/dist/bootstrap-vue.min.css');
+
+//     //wp_enqueue_style('fgproduct-styles', plugins_url('/css/styles.css', __FILE__ ));
+// }
+// add_action('admin_enqueue_scripts', 'ajdt_enqueue_vuejs_styles');
+
+// function ajdt_enqueue_vuejs_scripts(){
+//     wp_enqueue_script('polyfill', '//polyfill.io/v3/polyfill.min.js?features=es2015%2CIntersectionObserver', [], '');
+//     wp_enqueue_script('vue', '//unpkg.com/vue@latest/dist/vue.min.js', [], '');
+//     wp_enqueue_script('bootstrap-vue', '//unpkg.com/bootstrap-vue@latest/dist/bootstrap-vue.min.js', [], '');
+//     wp_enqueue_script('bootstrap-vue-icons', '//unpkg.com/bootstrap-vue@latest/dist/bootstrap-vue-icons.min.js', [], '');
+//     wp_enqueue_script('Utils-details', plugin_dir_url( __FILE__ ) . 'vueapp.js', [], '1.0', true);
+// }
+// add_action('admin_enqueue_scripts', 'ajdt_enqueue_vuejs_scripts' );
+
+// function ajdt_enqueue_test_scripts(){
+//     wp_register_style( 'Font_Awesome', 'https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css' );
+//     wp_enqueue_style('Font_Awesome');
+// }
+// add_action('admin_enqueue_scripts', 'ajdt_enqueue_test_scripts' );
+
+
+//[AjaxTableUtil] - checks for shortcode in wordpress and renders this div mount
+function handle_shortcode() {
+    return '<div id="mount"></div>';
+}
+add_shortcode('AjaxTableUtil', 'handle_shortcode'); 
+
+function vueAdminPage() {
+  add_menu_page('AjaxTable Settings', 'AjaxTable Settings', 'manage_options' ,__FILE__, 'RenderAjaxTable', 'dashicons-forms');
+}
+add_action('admin_menu', 'vueAdminPage');
+
+function RenderAjaxTable(){
+    echo handle_shortcode();
+}
---
+++ class-assets.php
@@ -0,0 +1,174 @@
+<?php
+namespace AjaxTable;
+
+/**
+ * Scripts and Styles Class
+ */
+class Assets {
+
+    function __construct() {
+        if ( is_admin() ) {
+            add_action( 'admin_enqueue_scripts', [ $this, 'register' ], 5 );
+        } else {
+            add_action( 'wepos_enqueue_scripts', [ $this, 'register' ], 5 );
+        }
+    }
+
+    /**
+     * Register our app scripts and styles
+     *
+     * @return void
+     */
+    public function register() {
+        $this->register_scripts( $this->get_scripts() );
+        $this->register_styles( $this->get_styles() );
+        $this->enqueue_all_scripts();
+        $this->register_localize();
+    }
+
+    /**
+     * Register scripts
+     *
+     * @param  array $scripts
+     *
+     * @return void
+     */
+    private function register_scripts( $scripts ) {
+        foreach ( $scripts as $handle => $script ) {
+            $deps      = isset( $script['deps'] ) ? $script['deps'] : null;
+            $in_footer = isset( $script['in_footer'] ) ? $script['in_footer'] : false;
+            $version   = isset( $script['version'] ) ? $script['version'] : null;
+            wp_register_script( $handle, $script['src'], $deps, $version, $in_footer );
+        }
+    }
+
+    /**
+     * Register styles
+     *
+     * @param  array $styles
+     *
+     * @return void
+     */
+    public function register_styles( $styles ) {
+        foreach ( $styles as $handle => $style ) {
+            //$deps = isset( $style['deps'] ) ? $style['deps'] : false;
+            //wp_register_style( $handle, $style['src'], $deps, AJDT_VERSION );
+            wp_register_style( $handle, $style['src'] );
+        }
+    }
+
+    /**
+     * Get all registered scripts
+     *
+     * @return array
+     */
+    public function get_scripts() {
+        global $wp_version;
+
+        $scripts = [
+            'polyfill' => [
+                'src'       => '//polyfill.io/v3/polyfill.min.js?features=es2015%2CIntersectionObserver',
+                'in_footer' => true
+            ],
+            'vue' => [
+                'src'       => '//unpkg.com/vue@latest/dist/vue.min.js',
+                'in_footer' => true
+            ],
+            'bootstrap-vue' => [
+                'src'       => '//unpkg.com/bootstrap-vue@latest/dist/bootstrap-vue.min.js',
+                'in_footer' => true
+            ],
+            'bootstrap-vue-icons' => [
+                'src'       => '//unpkg.com/bootstrap-vue@latest/dist/bootstrap-vue-icons.min.js',
+                'in_footer' => true
+            ],
+            'ajdt-lib' => array(
+                'src'       => AJDT_URL . '/vueapp.js',
+                'in_footer' => true
+            )
+        ];
+
+        return $scripts;
+    }
+
+    /**
+     * Get registered styles
+     *
+     * @return array
+     */
+    public function get_styles() {
+
+        $styles = [
+            'bootstrap-vue' => [
+                'src' =>  '//unpkg.com/bootstrap/dist/css/bootstrap.min.css'
+            ],
+            'bootstrap-vue-min' => [
+                'src' =>  '//unpkg.com/bootstrap-vue@latest/dist/bootstrap-vue.min.css'
+            ]
+        ];
+
+        return $styles;
+    }
+
+    public function enqueue_all_scripts() {
+       //if ( ! is_admin() ) {
+            // Enqueue all style
+            wp_enqueue_style( 'bootstrap-vue' );
+            wp_enqueue_style( 'bootstrap-vue-min' );
+
+            // Load scripts
+            wp_enqueue_script( 'polyfill' );
+            wp_enqueue_script( 'vue' );
+            wp_enqueue_script( 'bootstrap-vue' );
+            wp_enqueue_script( 'bootstrap-vue-icons' );
+
+            do_action( 'ajdt_load_forntend_scripts' );
+            wp_enqueue_script( 'ajdt-lib' );
+       // }
+    }
+
+    /**
+     * Set localize script data
+     *
+     * @since 1.0.0
+     *
+     * @return void
+     */
+    public function register_localize() {
+        $localize_data = apply_filters( 'adjt_localize_data', [
+            'rest' => array(
+                'privusername'    => 'shajeeb shahul hameed',
+                'root'    => esc_url_raw( get_rest_url() ),
+                'nonce'   => wp_create_nonce( 'wp_rest' ),
+                'wcversion' => 'wc/v3',
+                'posversion' => 'adjt/v1',
+            ),
+            'ajaxurl'                      => admin_url( 'admin-ajax.php' ),
+            'nonce'                        => wp_create_nonce( 'wepos_nonce' ),
+            'libs'                         => [],
+            'routeComponents'              => array( 'default' => null ),
+            'assets_url'                   => AJDT_ASSETS,
+            'ajax_loader'                  => AJDT_ASSETS . '/images/spinner-2x.gif',
+            'logout_url'                   => wp_logout_url( site_url() )
+        ] );
+
+        wp_localize_script( 'adjt-vendor', 'adjt', $localize_data );
+    }
+
+     /**
+     * SPA Routes
+     *
+     * @return array
+     */
+    public function get_vue_admin_routes() {
+        $routes = array(
+            array(
+                'path'      => '/settings',
+                'name'      => 'Settings',
+                'component' => 'Settings'
+            ),
+        );
+
+        return apply_filters( 'wepos_admin_routes', $routes );
+    }
+}
